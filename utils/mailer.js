529ccafe9f54158cf34b38cd8f371ac3b89a3f6e
VoDucTri <144577759+VoDucTri@users.noreply.github.com>
2025-04-03 14:05:54 +0700

    update code bai tap

---
+++ utils/mailer.js
@@ -0,0 +1,21 @@
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "61113dde2f96a8",
+        pass: "ade4a175e8e632",
+    },
+});
+
+module.exports = {
+    sendmailFrogetPass: async function (to, URL) {
+        return await transporter.sendMail({
+            from: `NNPTUD@heeheheh`, // sender address
+            to: to, // list of receivers
+            subject: "MAIL MOI DU LICH CAM", // Subject line
+            html: `<a href=${URL}>CLICK VAO DAY DE XEM VIEC NHE VOLT CAO</a>`, // html body
+        });
+    }
+}
